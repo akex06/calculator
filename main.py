4dbaee1991b507b542b3f250b68ac108597e83ca
Alessandro <alessandro@akex.dev>
2023-11-10 10:40:35 +0100

    Functions and parenthesis implemented

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import math
 import operator
-from typing import Literal
+from typing import Literal, Any
+from collections.abc import MutableSequence
 
-queue: list = list()
-operator_stack: list = list()
 
+class Stack(list):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def popto(self, mutable_sequence: MutableSequence) -> Any:
+        mutable_sequence.append(self.pop())
 
 class Operator:
     def __init__(
@@ -18,6 +24,9 @@ class Operator:
         self.associativity = associativity
 
 
+queue: Stack = Stack()
+operator_stack: Stack = Stack()
+
 operators: dict[str, Operator] = {
     "**": Operator("**", 4, "right", operator.pow),
     "*": Operator("*", 3, "right", operator.mul),
@@ -26,10 +35,24 @@ operators: dict[str, Operator] = {
     "+": Operator("**", 2, "right", operator.add),
 }
 
-operation: str = "3 + 4 - 5 / 6"
+functions: dict[str, callable] = {
+    "sin": math.sin,
+    "cos": math.cos,
+    "tan": math.tan,
+    "abs": abs,
+    "int": int,
+    "max": max,
+    "min": min
+}
+
+operation: str = "sin ( max ( 2 , 3 ) / 3 * 3"
 for token in operation.split():
     if token.isnumeric():
         queue.append(int(token))
+        continue
+
+    if token in functions:
+        operator_stack.append(token)
         continue
 
     if token in operators:
@@ -42,8 +65,8 @@ for token in operation.split():
             if (
                     o2.precedence > o1.precedence
                     or (
-                        o1.precedence == o2.precedence
-                        and o1.associativity == "left")
+                    o1.precedence == o2.precedence
+                    and o1.associativity == "left")
             ):
                 queue.append(operator_stack.pop())
                 continue
@@ -51,6 +74,24 @@ for token in operation.split():
             break
 
         operator_stack.append(token)
+
+    if token == ",":
+        while operator_stack[-1] != "(":
+            queue.append(operator_stack.pop())
+
+    if token == "(":
+        operator_stack.append(token)
+        continue
+
+    if token == ")":
+        while operator_stack[-1] != "(":
+            assert len(operator_stack) > 0, "The operator stack is empty"
+            queue.append(operator_stack.pop())
+
+        assert operator_stack[-1] == "(", "The operator stack should have a left parenthesis at the top"
+        operator_stack.pop()
+        if operator_stack[-1] in functions:
+            queue.append(operator_stack.pop())
 
 queue.extend(operator_stack[::-1])
 print(queue)
